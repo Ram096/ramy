fd158656c01650787cbcb06b85c83a207b2a7a0d
Ramy Kassam <Raka0023@colorado.edu>
2024-09-20 13:56:54 -0600

    margins

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -1,28 +1,34 @@
 import React from 'react';
-import resume from '../assets/documents/RAMY_KASSAM_RESUME.pdf'
+import resume from '../assets/documents/RAMY_KASSAM_RESUME.pdf';
+import "../styles/Resume.css"
 
-const resumeUrl = resume; 
+const resumeUrl = resume;
 
 const Resume = () => {
   return (
-    <div style={{ padding: '20px'}}>
-      <embed 
-        src={resumeUrl} 
-        type="application/pdf" 
-        width="100%" 
-        height="600px" 
-        style={{ border: 'none' }}
-        aria-label="Resume"
-      />
-      <div style={{ marginTop: '20px' }}>
-        <a 
-          href={resumeUrl} 
-          download="Ramy_Kassam_Resume.pdf" 
-          style={{ fontSize: '16px', textDecoration: 'none', color: '#007BFF' }}
-        >
-          Download Resume
-        </a>
+    <div className="resume-page">
+      <div className="resume-container">
+        <embed 
+          src={resumeUrl} 
+          type="application/pdf" 
+          width="100%" 
+          height="600px" 
+          style={{ border: 'none' }}
+          aria-label="Resume"
+        />
+        <div style={{ marginTop: '20px' }}>
+          <a 
+            href={resumeUrl} 
+            download="Ramy_Kassam_Resume.pdf" 
+            style={{ fontSize: '16px', textDecoration: 'none', color: '#007BFF' }}
+          >
+            Download Resume
+          </a>
+        </div>
       </div>
+      <footer className="footer-container">
+        {/* Footer content goes here */}
+      </footer>
     </div>
   );
 };
